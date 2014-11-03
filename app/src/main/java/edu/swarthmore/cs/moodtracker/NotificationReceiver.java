c8a37e8bb776d7b2830e369519e248433d9cfcb7
cwang3 <cwang3@swarthmore.edu>
2014-11-03 16:03:38 -0500

    Notification Working?

--- app/src/main/java/edu/swarthmore/cs/moodtracker/NotificationReceiver.java
+++ app/src/main/java/edu/swarthmore/cs/moodtracker/NotificationReceiver.java
@@ -1,16 +1,19 @@
 package edu.swarthmore.cs.moodtracker;
 
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by cwang3 on 11/3/14.
  */
 public class NotificationReceiver extends BroadcastReceiver {
+    public static final String TAG = "NotificationReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive called");
         Intent service1 = new Intent(context, NotificationAlarmService.class);
         context.startService(service1);
     }
