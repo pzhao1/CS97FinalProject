2b996b5760b9d850455430c3be92e883bbe2dbb1
Richard Liang <wliang1@swarthmore.edu>
2014-11-04 15:50:11 -0500

    further work on layout

--- app/src/main/java/edu/swarthmore/cs/moodtracker/SurveySectionFragment.java
+++ app/src/main/java/edu/swarthmore/cs/moodtracker/SurveySectionFragment.java
@@ -5,7 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import edu.swarthmore.cs.moodtracker.util.SurveyArrayAdapter;
 
 /**
  * Created by Peng on 10/19/2014.
@@ -17,6 +23,29 @@ public class SurveySectionFragment extends Fragment {
      * fragment.
      */
 
+    private SurveyArrayAdapter mAdapter;
+    private ListView mListView;
+    private TextView mHeader;
+    private TextView mQuestionSubheader;
+
+    private static final String[] QUESTION_TEXT = {
+            "1 (Very Slightly or Not at All)" ,
+            "2 (A Little)",
+            "3 (Moderately)",
+            "4 (Quite a Bit)",
+            "5 (Extremely)"
+
+    };
+    private SurveyArrayAdapter.RowSelectionListener mSelectionListener = new SurveyArrayAdapter.RowSelectionListener() {
+        @Override
+        public void onRowSelection(final int row) {
+            if (mAdapter != null) {
+                mAdapter.setSelection(row);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
     public SurveySectionFragment() {
         super();
     }
@@ -25,10 +54,28 @@ public class SurveySectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_section_survey, container, false);
-
-        TextView labelTextView = (TextView) rootView.findViewById(R.id.section_label);
-        labelTextView.setText("This is the survey section");
+        mListView = (ListView) rootView.findViewById(android.R.id.list);
+        mHeader = (TextView) rootView.findViewById(R.id.section_header);
+        mQuestionSubheader = (TextView) rootView.findViewById(R.id.section_question);
 
         return rootView;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mHeader.setText("Example");
+        mQuestionSubheader.setText("Hello?");
+
+        if (mAdapter == null) {
+            List<String> options = Arrays.asList(QUESTION_TEXT);
+            mAdapter = new SurveyArrayAdapter(getActivity(), R.layout.list_item_survey, android.R.id.text1, options);
+            mAdapter.setRowSelectionListener(mSelectionListener);
+        }
+
+        mListView.setAdapter(mAdapter);
+    }
+
+
 }
--- app/src/main/java/edu/swarthmore/cs/moodtracker/util/SurveyArrayAdapter.java
+++ app/src/main/java/edu/swarthmore/cs/moodtracker/util/SurveyArrayAdapter.java
@@ -18,7 +18,7 @@ public class SurveyArrayAdapter extends ArrayAdapter<String>{
         public void onRowSelection(int row);
     }
 
-    private int mSelectedIndex;
+    private int mSelectedIndex = 0;
     private RowSelectionListener mListener;
 
     public SurveyArrayAdapter(Context context, int resource, int textViewResourceId, List<String> objects) {
