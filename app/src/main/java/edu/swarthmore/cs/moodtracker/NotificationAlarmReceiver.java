148321c92ae7aa4685d00bba0f9cdfb4ccb07c37
cwang3 <cwang3@swarthmore.edu>
2014-11-06 14:04:26 -0500

    notification no longer creates a new service

--- app/src/main/java/edu/swarthmore/cs/moodtracker/NotificationAlarmReceiver.java
+++ app/src/main/java/edu/swarthmore/cs/moodtracker/NotificationAlarmReceiver.java
@@ -1,12 +1,15 @@
 package edu.swarthmore.cs.moodtracker;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 /**
- * When it is time to take surveys, initiate a service that creates the notification.
+ * When it is time to take surveys, send a notification.
  */
 public class NotificationAlarmReceiver extends BroadcastReceiver {
     public static final String TAG = "NotificationReceiver";
@@ -14,8 +17,23 @@ public class NotificationAlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive called");
-        Intent service1 = new Intent(context, NotificationAlarmService.class);
-        context.startService(service1);
-        //context.stopService(service1);?? Do I need to stop the service?
+        NotificationManager mNotificationManager;
+        PendingIntent mPendingIntent;
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("MoodTracker")
+                        .setContentText("Take MoodTracker Survey!")
+                        .setAutoCancel(true);
+        // When user clicks the notification, open MoodTracker survey
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        resultIntent.putExtra(MainActivity.EXTRA_DRAWER_SELECT, 2);
+        mPendingIntent = PendingIntent.getActivity(context,0,resultIntent,PendingIntent.FLAG_CANCEL_CURRENT);
+        mBuilder.setContentIntent(mPendingIntent);
+        mBuilder.setNumber(0);
+
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
