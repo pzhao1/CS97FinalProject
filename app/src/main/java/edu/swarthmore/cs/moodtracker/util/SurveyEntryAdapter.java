f6db91a53c190698af5b210b0b648ebda56d574d
Richard Liang <wliang1@swarthmore.edu>
2014-12-09 22:15:11 -0500

    beginnings of styling survey section

--- app/src/main/java/edu/swarthmore/cs/moodtracker/util/SurveyEntryAdapter.java
+++ app/src/main/java/edu/swarthmore/cs/moodtracker/util/SurveyEntryAdapter.java
@@ -37,6 +37,9 @@ public class SurveyEntryAdapter extends ArrayAdapter<SurveyEntry> {
 
         SurveyEntry entry = getItem(position);
         if (entry != null) {
+            TextView numberField = (TextView) view.findViewById(android.R.id.text2);
+            numberField.setText("Survey Number :" + position);
+
             TextView dateField = (TextView) view.findViewById(android.R.id.text1);
             dateField.setText(mDateFormat.format(entry.getDate()));
         }
